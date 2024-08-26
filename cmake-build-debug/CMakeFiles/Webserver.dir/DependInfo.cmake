aae07f5aa37a2e21dad33db240d019403bb21e91
gsl <gsl15705500419@outlook.com>
2024-08-26 20:37:53 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/Webserver.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/msi/workspace/webserver/src/Buffer.cpp" "CMakeFiles/Webserver.dir/src/Buffer.cpp.o" "gcc" "CMakeFiles/Webserver.dir/src/Buffer.cpp.o.d"
+  "/home/msi/workspace/webserver/src/Channel.cpp" "CMakeFiles/Webserver.dir/src/Channel.cpp.o" "gcc" "CMakeFiles/Webserver.dir/src/Channel.cpp.o.d"
+  "/home/msi/workspace/webserver/src/Dispatcher.cpp" "CMakeFiles/Webserver.dir/src/Dispatcher.cpp.o" "gcc" "CMakeFiles/Webserver.dir/src/Dispatcher.cpp.o.d"
+  "/home/msi/workspace/webserver/src/EpollDispatcher.cpp" "CMakeFiles/Webserver.dir/src/EpollDispatcher.cpp.o" "gcc" "CMakeFiles/Webserver.dir/src/EpollDispatcher.cpp.o.d"
+  "/home/msi/workspace/webserver/src/EventLoop.cpp" "CMakeFiles/Webserver.dir/src/EventLoop.cpp.o" "gcc" "CMakeFiles/Webserver.dir/src/EventLoop.cpp.o.d"
+  "/home/msi/workspace/webserver/src/HttpRequest.cpp" "CMakeFiles/Webserver.dir/src/HttpRequest.cpp.o" "gcc" "CMakeFiles/Webserver.dir/src/HttpRequest.cpp.o.d"
+  "/home/msi/workspace/webserver/src/Httpresponse.cpp" "CMakeFiles/Webserver.dir/src/Httpresponse.cpp.o" "gcc" "CMakeFiles/Webserver.dir/src/Httpresponse.cpp.o.d"
+  "/home/msi/workspace/webserver/src/PollDispatcher.cpp" "CMakeFiles/Webserver.dir/src/PollDispatcher.cpp.o" "gcc" "CMakeFiles/Webserver.dir/src/PollDispatcher.cpp.o.d"
+  "/home/msi/workspace/webserver/src/SelectDispatcher.cpp" "CMakeFiles/Webserver.dir/src/SelectDispatcher.cpp.o" "gcc" "CMakeFiles/Webserver.dir/src/SelectDispatcher.cpp.o.d"
+  "/home/msi/workspace/webserver/src/TcpConnection.cpp" "CMakeFiles/Webserver.dir/src/TcpConnection.cpp.o" "gcc" "CMakeFiles/Webserver.dir/src/TcpConnection.cpp.o.d"
+  "/home/msi/workspace/webserver/src/TcpServer.cpp" "CMakeFiles/Webserver.dir/src/TcpServer.cpp.o" "gcc" "CMakeFiles/Webserver.dir/src/TcpServer.cpp.o.d"
+  "/home/msi/workspace/webserver/src/ThreadPool.cpp" "CMakeFiles/Webserver.dir/src/ThreadPool.cpp.o" "gcc" "CMakeFiles/Webserver.dir/src/ThreadPool.cpp.o.d"
+  "/home/msi/workspace/webserver/src/WorkerThread.cpp" "CMakeFiles/Webserver.dir/src/WorkerThread.cpp.o" "gcc" "CMakeFiles/Webserver.dir/src/WorkerThread.cpp.o.d"
+  "/home/msi/workspace/webserver/src/chengxu.cpp" "CMakeFiles/Webserver.dir/src/chengxu.cpp.o" "gcc" "CMakeFiles/Webserver.dir/src/chengxu.cpp.o.d"
+  "/home/msi/workspace/webserver/src/main.cpp" "CMakeFiles/Webserver.dir/src/main.cpp.o" "gcc" "CMakeFiles/Webserver.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
